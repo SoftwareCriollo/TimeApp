cd7b19782ee5c390475a0ab2d44ebad852e4c427
Daniel Z <daniel@softwarecriollo.com>
2014-12-22 13:06:50 -0430

    Saving user model

--- app/assets/javascripts/time_angular/models/user.js
+++ app/assets/javascripts/time_angular/models/user.js
@@ -1,5 +1,10 @@
 (function(){
   timeApp = windows.timeApp ||= {};
-  timeApp.User = function(){};
+
+  timeApp.User = function(){
+    this.email = "";
+    this.name = "";
+    this.rank = "developer";
+  };
 
 })();
