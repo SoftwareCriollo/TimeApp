d4b6c691874a754cc03d5f3fbd05ad57ff31f2ad
Daniel Z <daniel@softwarecriollo.com>
2014-12-25 18:55:13 -0430

    Serializers

--- app/models/concerns/serializer.rb
+++ app/models/concerns/serializer.rb
@@ -1,7 +1,11 @@
 module Serializer
   def arrays_object(array,attrs)
+    attrs = attrs.map(&:to_sym)
     array.map do |item|
-      item.attributes.delete_if{ |key,value| !attrs.include?(key) }
+      attributes = item.attributes.delete_if do |key,value|
+        !attrs.include?(key.to_sym)
+      end
+      attributes.symbolize_keys
     end
   end
 end
