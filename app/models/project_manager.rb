94813ea1cc083aa22e556e7ad332c65ef4b75aa8
Daniel Z <daniel@softwarecriollo.com>
2014-12-26 13:05:47 -0430

    looking cards for one board

--- app/models/project_manager.rb
+++ app/models/project_manager.rb
@@ -1,7 +1,9 @@
 class ProjectManager
   include Serializer
 
-  BOARD_ATTRIBUTES= [:id,:name,:description,:url]
+  BOARD_ATTRIBUTES= [:id,:name,:url]
+
+  CARDS_ATTRIBUTES= [:id, :name, :url]
 
   def initialize
     @organization = Trello::Organization.find(organization_name)
@@ -11,8 +13,19 @@ class ProjectManager
     @organization.boards
   end
 
+  def cards_by_board(board_id)
+    board = Trello::Board.find(board_id)
+    board.cards
+  end
+
+
   def boards_serialized
     arrays_object(boards,BOARD_ATTRIBUTES)
+  end
+
+  def cards_serialized(board_id)
+    cards = cards_by_board(board_id)
+    arrays_object(cards,CARDS_ATTRIBUTES)
   end
 
   def organization_name
