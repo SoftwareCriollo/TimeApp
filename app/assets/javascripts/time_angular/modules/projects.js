40cebb8bbff9996153e4619b2de52abf6cbac4fd
genesis <genesisgonza05@gmail.com>
2015-01-07 20:09:18 -0430

    style time log

--- app/assets/javascripts/time_angular/modules/projects.js
+++ app/assets/javascripts/time_angular/modules/projects.js
@@ -27,6 +27,43 @@
         cardsCache.saveCards(projectId,cards);
         controller.cards = cards;
       });
+    
+    this.task = [];
+    this.tasks=function(idCard, nameCard){
+      var dateCards = {};
+      dateCards.id = idCard;
+      dateCards.name = nameCard;
+      var add = 0;
+      if(this.task!="") {
+        for (var i = 0; i < this.task.length; i++) {
+          if (this.task[i].id==idCard)
+            {
+              add=add+1;
+              var remove = i;
+            }
+        }
+        if(add==0){this.task.push(dateCards);}
+        else{this.task.splice(remove,1);}
+        add=0;
+      }
+      else{
+        this.task.push(dateCards);
+      }
+      if(this.task!=""){
+          $('.save-time-log').css("display","block");
+          $('.title-time-log').css("display","inline-block");
+          $('.underline-time-log').css("display","block");
+        }
+      else{
+          $('.title-time-log').css("display","none");
+          $('.save-time-log').css("display","none");
+          $('.underline-time-log').css("display","none");
+        }
+    };
 
   }]);
 })();
+
+
+
+    
