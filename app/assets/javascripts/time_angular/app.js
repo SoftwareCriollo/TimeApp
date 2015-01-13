804e4d9eab986f382aa36347dfc682ac1ade524d
genesis <genesisgonza05@gmail.com>
2015-01-13 11:11:40 -0430

    start slice general performance

--- app/assets/javascripts/time_angular/app.js
+++ app/assets/javascripts/time_angular/app.js
@@ -44,6 +44,9 @@
       .when('/iterations/:projectId', {
         templateUrl: 'templates/iterations/index.html'
       })
+      .when('/performance', {
+        templateUrl: 'templates/performance/index.html'
+      })
       .otherwise({
         redirectTo: '/projects'
       });
