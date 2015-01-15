2edebd89c3fe074b8f427b0577342aff940fc37c
genesis <genesisgonza05@gmail.com>
2015-01-15 13:13:22 -0430

    time view entries with functionalities
    functionalities

--- app/assets/javascripts/time_angular/modules/iterations.js
+++ app/assets/javascripts/time_angular/modules/iterations.js
@@ -9,7 +9,27 @@
     var controller = this;
     var projectId = $routeParams.projectId;
 
-    /* controller in construction */
+    this.project = projectCache.findProject(projectId);
+
+    this.editTimeEntry = function(entry){
+
+      if(this.lastEntry){
+        $('.card-iterarion-' + this.lastEntry).removeClass("selected"); //add class selected to card by entry (entry's the id), necessary to build the id
+        $('.entry-time-' + this.lastEntry).addClass("hide");
+        $('.edit-entry-' + this.lastEntry).removeClass("hide");
+        $('.entry-time-iterations-' + this.lastEntry).removeClass("margin-show-entry-edit");
+      }
+
+      $('.card-iterarion-' + entry).addClass("selected"); //add class selected to card by entry (entry's the id), necessary to build the id
+      $('.entry-time-' + entry).removeClass("hide");
+      $('.edit-entry-' + entry).addClass("hide");
+      $('.entry-time-iterations-' + entry).addClass("margin-show-entry-edit");
+
+
+      this.lastEntry = entry;
+    };
+
+
 
   }]);
 
