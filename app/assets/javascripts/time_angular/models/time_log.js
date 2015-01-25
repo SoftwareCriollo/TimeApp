cf48f91f446a092904eb8ed219671ef7039fc4f0
Daniel Z <daniel@softwarecriollo.com>
2015-01-25 13:33:27 -0430

    Refactoring methods timelogger

--- app/assets/javascripts/time_angular/models/time_log.js
+++ app/assets/javascripts/time_angular/models/time_log.js
@@ -1,19 +1,58 @@
 (function(){
-
   window.TimeApp =  window.TimeApp || {};
-  var TimeApp = window.TimeApp;
-  var timeLog = TimeApp.timeLog = {};
-
-  timeLog = function(attributes){
+  var TimeLogger = function(attributes){
     attributes = attributes || {};
     this.project_id = attributes.project_id;
     this.project_name = attributes.project_name;
-    this.time_logs = attributes.time_logs;
+    this.timelogs = attributes.timelogs || [];
 
-    this.toJsonString = function(){
-      this.attributes;
+    this.toJson = function(){
+      return {
+        "project_id":this.project_id,
+        "project_name":this.project_name,
+        "timelogs": this.time_logs,
+      };
+    };
+    this.hasTask = function(){
+      return this.timelogs.length > 0;
+    };
+
+    this.toggleTasks = function(card){
+      if(this.isSelected(card))
+        this.removeCard(card);
+      else
+        this.addCard(card);
+    };
+
+    this.addCard = function(card){
+      var dataTask = {"task_id":card.id, "task_name": card.name };
+      this.timelogs.push(dataTask);
+    };
+
+    this.removeCard = function(card){ //function to remove a card in arrat tasks
+      this.timelogs.splice(this.searchCard(card),1);
+    };
+
+    this.isSelected = function(card){
+      return (this.searchCard(card) >= 0);
+    };
+
+    this.searchCard = function(card){
+      var task = undefined;
+      for (var i = this.timelogs.length - 1; i >= 0; i--) {
+        task = this.timelogs[i];
+        if(equals(task,card))
+          return i;
+      };
+      return -1;
+    }
+    // Privates functions
+    var equals = function(task,card){
+      return task.task_id == card.id
     };
   };
+
+  window.TimeApp.TimeLogger = TimeLogger;
 })();
 
 
--- app/assets/javascripts/time_angular/modules/projects.js
+++ app/assets/javascripts/time_angular/modules/projects.js
@@ -1,5 +1,6 @@
 'use strict';
 (function(){
+  var TimeApp = window.TimeApp;
   var app = angular.module('timeFrontendApp-projects',['CacheStore','Repository'])
 
   app.controller('ProjectsController',['ProjectRepository','CurrentUser','ProjectCache', function(projectRepository,currentUser,projectsCache){
@@ -8,12 +9,11 @@
       localStorage.clear();
       location.reload();
     };
-
     currentUser.isPendingAuth();
 
     var controller = this;
-
     this.projects = projectsCache.projects;
+
     projectRepository.get(function(projects, status, headers, config){
       projectsCache.saveProjects(projects);
       controller.projects = projects;
@@ -25,15 +25,14 @@
 
     var controller = this;
     var projectId = $routeParams.projectId;
+    this.timeLogger = new TimeApp.TimeLogger();
+
     currentUser.isPendingAuth();
 
     this.project = projectCache.findProject(projectId);
     this.cards = cardsCache.loadCards(projectId);
-    this.task = [];
-    this.indexRemove = null;
 
     cardRepository.setProjectId(projectId);
-
     cardRepository.get(function(cards, status, headers, config){
       cardsCache.saveCards(projectId,cards);
       controller.cards = cards;
@@ -44,52 +43,6 @@
       location.reload();
     };
 
-    /* add the selected card in an array, the array would be the "pre time log"  */
-    this.isUnSelected = function(idCard, nameCard){ // function to validate is unselected a card
-      for (var i = 0; i < this.task.length; i++) {
-        if (this.task[i].id==idCard) {
-            this.indexRemove = i;
-            return true;
-          }
-        }
-    };
-
-    this.removeCard = function(idCard, nameCard){ //function to remove a card in arrat tasks
-      this.task.splice(this.indexRemove,1);
-      this.indexRemove = null;
-    };
-
-    this.addCard = function(idCard, nameCard){  // function to add card in array tasks
-      var dateCards = {};
-      dateCards.id = idCard;
-      dateCards.name = nameCard;
-      this.task.push(dateCards);
-    };
-
-    this.tasks = function(idCard, nameCard){ // function to do array time log
-      if(this.task.length>0 && this.isUnSelected(idCard, nameCard)){
-        this.removeCard(idCard, nameCard);
-      }
-      else if(this.task.length>0 && !this.isUnSelected(idCard, nameCard)){
-        this.addCard(idCard, nameCard);
-      }
-      else{
-        this.addCard(idCard, nameCard);
-      }
-
-      if(this.task.length>0){
-        $('.section-time-log').removeClass('hide');
-        $('.underline-division').removeClass('hide');
-      }
-      else{
-        $('.section-time-log').addClass('hide');
-        $('.underline-division').addClass('hide');
-      }
-    };
-  /************************************************************************************/
-
   }]);
-
-
 })();
 
