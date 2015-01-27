a9c993b4ab5a3440e5cb76438f0fdc604c8cffee
Rafael Camacho <rafael@softwarecriollo.com>
2015-01-26 21:01:08 -0430

    iteration model

---
+++ app/assets/javascripts/time_angular/models/iteration.js
@@ -0,0 +1,17 @@
+(function(){
+  window.TimeApp =  window.TimeApp || {};
+  var Iteration = function(attributes){
+    attributes = attributes || {} 
+    this.project_id = attributes.project_id;
+    this.time = attributes.time;
+    this.note = attributes.note;
+    this.start = attributes.start;
+    this.end_date = attributes.end_date;
+    this.invoice = attributes.invoice;
+    
+    this.toJson = function(){
+      return attributes.toJson();
+    };
+  };
+  window.TimeApp.Iteration = Iteration;
+})();
