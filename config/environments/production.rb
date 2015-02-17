d814c0dd353fbbd3f3a2c86246a50f55af76b512
Daniel Z <daniel@softwarecriollo.com>
2015-02-16 23:18:08 -0430

    email production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,13 +16,14 @@ Rails.application.configure do
   config.autoflush_log = false
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'time-criollo-staging.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: ENV["GMAIL_DOMAIN"],
     authentication: "plain",
-    enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
   }
